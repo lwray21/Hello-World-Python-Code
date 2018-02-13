568e8accbf567ee2a65655a157e5687b81569396
lwray21 <36418949+lwray21@users.noreply.github.com>
2018-02-12 19:20:23 -0500

    Add files via upload

---
+++ Hello python.py
@@ -0,0 +1,7 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print "Hello World!"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Hello World!")?
+>>> print ("Hello World!")
+Hello World!
+>>> 
